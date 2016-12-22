62e0b57e6ebc54c1cfba0a83f73449da55fe4b4c
nickverschoor <nick@verschoor.cc>
2016-12-22 10:07:32 +0100

    test

--- src/octoprint/util/comm.py
+++ src/octoprint/util/comm.py
@@ -990,9 +990,6 @@ class MachineCom(object):
 					except NameError:
 						old_output = "9999"
 
-					if old_output == "9999" and debugging_output == "0000":
-						self._changeState(self.STATE_OPERATIONAL)
-
 					self._do_send_without_checksum("status_res") #TODO: Test it!
 
 					if old_output != debugging_output:
@@ -1030,8 +1027,8 @@ class MachineCom(object):
 
 					old_output = debugging_output
 
-				else:
-					continue
+				#else:
+				#	continue
 
 				def convert_line(line):
 					if line is None:
